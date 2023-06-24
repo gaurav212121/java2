80988be5214f78e7372db906ab2022ca793a2e7b
gaurav212121 <shristirawat1234@gmail.com>
2023-06-24 17:53:26 +0530

    first commit

---
+++ pattern2.java
@@ -0,0 +1,19 @@
+public class pattern2 {
+    public static void main(String[] args) {
+        int c = 100;
+        for (int i=0; i < c; i++) {
+            for (int j = 0; j < c; j++)
+            {
+                if (i==0 || j==0 || i==c-1|| j==c-1)
+                {
+                    System.out.print("!");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
